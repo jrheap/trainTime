3bf9dc7e3feb2431ed57cb3a5cd2dc5c9b7a4fdf
Jason <jasonrheap@gmial.com>
2018-06-07 22:45:29 -0600

    10:45

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,7 +42,7 @@ $("#submitBtn").on("click", function(){
 
 //  Next step is to store them on the firebase
 database.ref().push({
- train: trainName,
+ Train: trainName,
  Destination: dest,
  First_train_time: firstTrain,
  Frequency: fre,
@@ -53,15 +53,50 @@ var form = document.getElementById("myForm");
     form.reset();
 })
 // Next we need to grab what is stored in the firebase and create the table with the info
-database.ref().on("child_added", function(snapshot) {
-  console.log(snapshot.val());
+
+  database.ref().on("child_added", function(snapshot) {
+    console.log(snapshot.val().Destination, snapshot.val().First_train_time, snapshot.val().Frequency, snapshot.val().Train);
+    // Assumptions
+    var tFrequency = snapshot.val().Frequency;
+    // Time is 3:30 AM
+    var firstTime = snapshot.val().First_train_time;
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    var nextTrainArival = moment(nextTrain).format("hh:mm")
+  
+  
+  
+    var newRow = $("<tr id='" + snapshot.val().Train + "'>")
+    newRow.prepend("<td>" + snapshot.val().Train + "</td>" + "<td>" + snapshot.val().Destination + "</td>" + "<td>" + snapshot.val().Frequency + "</td>" + "<td>" + nextTrainArival + "</td>" +
+    "<td>" + tMinutesTillTrain + "</td>" + "<td button id='delete' data-train='" + snapshot.val().Train + "'>X</td>");
+  // "<td>" + snapshot.val().First_train_time + "</td>"
+    $("tbody").prepend(newRow);
+  });
+  
+  $('.table').on('click', '#delete', function(event){
+    event.preventDefault()
+    console.log("clicked");
+    var trainName = $(this).attr('data-train') 
+    console.log(trainName);
+    database.ref().child(trainName).removevalue()
+  })
 
 
+  
 
-  var newRow = $("<tr>")
-  newRow.prepend("<td>" + snapshot.val().train + "</td>" + "<td>" + snapshot.val().Destination + "</td>" +
-  "<td>" + snapshot.val().First_train_time + "</td>" + "<td>" + "" + "</td>" + "<td>" + snapshot.val().Frequency + "</td>" +
-  "<td>" + "" + "</td>");
-
-  $("tbody").prepend(newRow);
-});
